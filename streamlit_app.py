68182a8594819f59089710f20a1ff524d5aaf856
xavidivekite <61840440+xavidivekite@users.noreply.github.com>
2025-01-30 16:15:09 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,8 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
+name_on_order = st.text_input('Name on smoothie:')
+
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
     my_dataframe
